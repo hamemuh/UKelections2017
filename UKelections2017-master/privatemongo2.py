63d7672d3ffcea6f13ad377f3b0ba720ca7b99e8
Muhammed Abdul-Hameed <muhammed.ah@hotmail.com>
2017-05-11 09:45:40 +0100

    Delete privatemongo2.py

--- UKelections2017-master/privatemongo2.py
+++
@@ -1,4 +0,0 @@
-TWITTER_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-TWITTER_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-TWITTER_APP_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-TWITTER_APP_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
